2807da79e84de583eb16b0f7f17ab51250b8a4c7
vicariousdrama <vdgit@lucasmoten.com>
2021-07-30 21:02:45 -0500

    Update display for Blocks Mined

--- scripts/difficultyepoch.py
+++ scripts/difficultyepoch.py
@@ -28,29 +28,46 @@ def getfont(size):
     if size == 24:
         return fontDeja24
 
-def drawcenteredtext(draw, s, fontsize, x, y):
+def drawcenteredtext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
     thefont = getfont(fontsize)
     sw,sh = draw.textsize(s, thefont)
     ox,oy = thefont.getoffset(s)
     sw += ox
     sh += oy
-    draw.text(xy=(x-(sw/2),y-(sh/2)), text=s, font=thefont, fill=colorFFFFFF)
+    draw.text(xy=(x-(sw/2),y-(sh/2)), text=s, font=thefont, fill=textcolor)
 
-def drawbottomlefttext(draw, s, fontsize, x, y):
+def drawbottomlefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
     thefont = getfont(fontsize)
     sw,sh = draw.textsize(s, thefont)
     ox,oy = thefont.getoffset(s)
     sw += ox
     sh += oy
-    draw.text(xy=(x,y-sh), text=s, font=thefont, fill=colorFFFFFF)
+    draw.text(xy=(x,y-sh), text=s, font=thefont, fill=textcolor)
 
-def drawbottomrighttext(draw, s, fontsize, x, y):
+def drawbottomrighttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
     thefont = getfont(fontsize)
     sw,sh = draw.textsize(s, thefont)
     ox,oy = thefont.getoffset(s)
     sw += ox
     sh += oy
     draw.text(xy=(x-sw,y-sh), text=s, font=thefont, fill=colorFFFFFF)
+
+def drawtoplefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
+    thefont = getfont(fontsize)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x,y), text=s, font=thefont, fill=textcolor)
+
+def drawtoprighttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
+    thefont = getfont(fontsize)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x-sw,y), text=s, font=thefont, fill=textcolor)
+
 
 def getcurrentblock():
     cmd = "bitcoin-cli getblockchaininfo"
@@ -99,11 +116,42 @@ def createimage(width=480, height=320):
     secondspassed = timenow - timebegan
     expectedmined = int(math.floor(secondspassed / 600))
     nextadjustment = str(float("%.2f" % (((float(blocksmined) / float(expectedmined)) - 1.0) * 100)))
+    adjustcolor = colorbehind
     if "-" not in nextadjustment:
         nextadjustment = "+" + nextadjustment
+        adjustcolor = colormined
+    estimateepochend = int(math.floor((float(secondspassed) / float(blocksmined))*2016)) + timebegan
+    secondstoepochend = estimateepochend - timenow
+    nextepochdesc = ""
+    if secondstoepochend > 86400:
+        nextepochdays = math.floor(secondstoepochend / 86400)
+        nextepochdesc = nextepochdesc + str(nextepochdays) + " day"
+        if nextepochdays > 1:
+            nextepochdesc = nextepochdesc + "s"
+        secondstoepochend = secondstoepochend - (nextepochdays * 86400)
+    if secondstoepochend > 3600:
+        nextepochhours = math.floor(secondstoepochend / 3600)
+        if nextepochdesc != "":
+            nextepochdesc = nextepochdesc + ", "
+        nextepochdesc = nextepochdesc + str(nextepochhours) + " hour"
+        if nextepochhours > 1:
+            nextepochdesc = nextepochdesc + "s"
+        secondstoepochend = secondstoepochend - (nextepochhours * 3600)
+    if secondstoepochend > 600:
+        nextepochminutes = math.floor(secondstoepochend / 600)
+        if nextepochdesc != "":
+            nextepochdesc = nextepochdesc + ", "
+        nextepochdesc = nextepochdesc + str(nextepochminutes) + " minute"
+        if nextepochminutes > 1:
+            nextepochdesc = nextepochdesc + "s"
+        secondstoepochend = secondstoepochend - (nextepochminutes * 600)
+    else:
+        if nextepochdesc == "":
+            "a few minutes"
+
     blockw=int(math.floor(width/63))
     padleft=int(math.floor((width-(63*blockw))/2))
-    padtop=40
+    padtop=36
     im = Image.new(mode="RGB", size=(width, height))
     draw = ImageDraw.Draw(im)
     for dc in range(63):
@@ -124,9 +172,14 @@ def createimage(width=480, height=320):
                     outlinecolor = colorbehind
                 draw.rectangle(xy=((tlx,tly),(brx,bry)),fill=None,outline=outlinecolor)
     drawcenteredtext(draw, "Blocks Mined This Difficulty Epoch", 24, int(width/2), int(padtop/2))
-    drawcenteredtext(draw, "Expected: " + str(expectedmined), 18, int(width/6*1), height-padtop)
-    drawcenteredtext(draw, "Mined: " + str(blocksmined), 18, int(width/6*3), height-padtop)
-    drawcenteredtext(draw, "Retarget: " + str(nextadjustment), 18, int(width/6*5), height-padtop)
+    drawtoprighttext(draw, "Expected: ", 18, int(width/4*1), height-56)
+    drawtoplefttext(draw, str(expectedmined), 18, int(width/4*1), height-56)
+    drawtoprighttext(draw, "Mined: ", 18, int(width/4*1), height-32)
+    drawtoplefttext(draw, str(blocksmined), 18, int(width/4*1), height-32)
+    drawtoprighttext(draw, "Retarget: ", 18, int(width/10*6), height-56)
+    drawtoplefttext(draw, str(nextadjustment) + "%", 18, int(width/10*6), height-56, adjustcolor)
+    drawtoprighttext(draw, "In: ", 18, int(width/10*6), height-32)
+    drawtoplefttext(draw, str(nextepochdesc), 18, int(width/10*6), height-32)
     drawbottomrighttext(draw, "as of " + getdateandtime(), 12, width, height)
     im.save(outputFile)
 
