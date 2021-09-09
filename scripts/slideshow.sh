a78b5d76a61678ad13cb04f498b1bbebaed22ea6
vicariousdrama <88121568+vicariousdrama@users.noreply.github.com>
2021-09-09 03:22:44 +0000

    Update slideshow.sh

--- scripts/slideshow.sh
+++ scripts/slideshow.sh
@@ -4,7 +4,7 @@
 # install fbi via  `sudo apt-get -y install fbi`
 while true
 do
-    fbi --vt 1 --autozoom --timeout 5 --device /dev/fb0 --noreadahead --cachemem 0 --noverbose --norandom /home/admin/images/* > /dev/null 2>&1
+    fbi --vt 1 --autozoom --timeout 5 --device /dev/fb0 --noreadahead --cachemem 0 --noverbose --norandom /home/bitcoin/images/* > /dev/null 2>&1
     sleep 120
     killall fbi > /dev/null 2>&1
 done
