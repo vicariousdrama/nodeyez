bfcb51c0088ef7108c9043333d87f81f80dd8443
vicariousdrama <88121568+vicariousdrama@users.noreply.github.com>
2021-12-02 18:33:24 +0000

    For Slushpool, make sats the standard

--- scripts/slushpool.py
+++ scripts/slushpool.py
@@ -8,6 +8,8 @@ import time
 
 outputFile = "/home/bitcoin/images/slushpool.png"
 authtoken = "your-auth-token-here"
+# if the daily average for 30 days drops below this level, then a warning will be displayed
+breakevndaily = 5000
 colorminingreward=ImageColor.getrgb("#6b50ff")
 colorbosreward=ImageColor.getrgb("#fb82a8")
 colorreferralreward=ImageColor.getrgb("#00bac5")
@@ -166,13 +168,14 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
     im = Image.new(mode="RGB", size=(width, height))
     draw = ImageDraw.Draw(im)
     # Header
-    drawcenteredtext(draw, "SlushPool Mining Pool Summary", 24, int(width/2), int(headerheight/2))
+    drawcenteredtext(draw, "SlushPool Mining Summary", 24, int(width/2), int(headerheight/2))
     # Hashrate and Unconfirmed rewards
     hashrate = getaccounthashrate(accountprofile)
     drawcenteredtext(draw, "Hashrate", 16, (width/4*1), (headerheight + (hashheight/2) - 24))
     drawcenteredtext(draw, hashrate, 24, (width/4*1), (headerheight + (hashheight/2)))
     unconfirmed_reward = float(accountprofile["btc"]["unconfirmed_reward"])
-    unconfirmed_text = str(format(unconfirmed_reward, ".8f")) + " BTC"
+#    unconfirmed_text = str(format(unconfirmed_reward, ".8f")) + " BTC"
+    unconfirmed_text = str(int(math.floor(unconfirmed_reward * 100000000))) + " sats"
     drawcenteredtext(draw, "Unconfirmed Rewards", 16, (width/4*3), (headerheight + (hashheight/2) - 24))
     drawcenteredtext(draw, unconfirmed_text, 24, (width/4*3), (headerheight + (hashheight/2)))
     # 30 Days Rewards
@@ -180,7 +183,7 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
     lowestreward = getlowestreward(accountrewards)
     labelwidth = math.floor(width / 5)
     graphedge = 3
-    drawtoplefttext(draw, "Rewards in last 30 days", 16, 0, (headerheight + hashheight))
+#    drawtoplefttext(draw, "Rewards in last 30 days", 16, 0, (headerheight + hashheight))
     charttop = headerheight + hashheight + 24
     chartleft = labelwidth + graphedge
     chartright = width - graphedge
@@ -199,27 +202,43 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
         draw.line(xy=[i,chart50,i+1,chart50],fill=colorgraphlinedark,width=1)
         draw.line(xy=[i,chart75,i+1,chart75],fill=colorgraphlinedark,width=1)
     # - left labels
-    reward25 = lowestreward + ((highestreward - lowestreward)/4*3)
-    reward50 = lowestreward + ((highestreward - lowestreward)/4*2)
-    reward75 = lowestreward + ((highestreward - lowestreward)/4*1)
-    drawrighttext(draw, str(format(reward25, ".5f")) + " BTC", 12, labelwidth, chart25)
-    drawrighttext(draw, str(format(reward50, ".5f")) + " BTC", 12, labelwidth, chart50)
-    drawrighttext(draw, str(format(reward75, ".5f")) + " BTC", 12, labelwidth, chart75)
+#    reward25 = lowestreward + ((highestreward - lowestreward)/4*3)
+#    reward50 = lowestreward + ((highestreward - lowestreward)/4*2)
+#    reward75 = lowestreward + ((highestreward - lowestreward)/4*1)
+#    drawrighttext(draw, str(format(reward25, ".5f")) + " BTC", 12, labelwidth, chart25)
+#    drawrighttext(draw, str(format(reward50, ".5f")) + " BTC", 12, labelwidth, chart50)
+#    drawrighttext(draw, str(format(reward75, ".5f")) + " BTC", 12, labelwidth, chart75)
+    reward25 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*3)) * 100000000))
+    reward50 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*2)) * 100000000))
+    reward75 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*1)) * 100000000))
+    drawrighttext(draw, str(reward25) + " sats", 12, labelwidth, chart25)
+    drawrighttext(draw, str(reward50) + " sats", 12, labelwidth, chart50)
+    drawrighttext(draw, str(reward75) + " sats", 12, labelwidth, chart75)
     # - 30 days of bars
+    totaldays = 30
     days = 0
-    daywidth = int(math.floor((chartright - chartleft) / 30))
+    daywidth = int(math.floor((chartright - chartleft) / totaldays))
     barwidth = daywidth - 2
+    overalltotal = 0
     for reward in accountrewards["btc"]["daily_rewards"]:
         days = days + 1
-        if days > 30:
+        if days > totaldays:
            break
         currenttotal = float(reward["total_reward"])
+        overalltotal = overalltotal + currenttotal
         dayx = chartright - (days * daywidth)
         barpct = 0
         if highestreward > lowestreward:
             barpct = (currenttotal-lowestreward)/(highestreward-lowestreward)
         bartop = chartbottom - int(math.floor((chartbottom-charttop)*barpct))
-        draw.rectangle(xy=[dayx,bartop,dayx+barwidth,chartbottom-1],fill=colorminingreward)
+        draw.rectangle(xy=[dayx,bartop,dayx+barwidth,chartbottom-1],fill=colorminingreward)       
+    overalltotal = overalltotal * 100000000
+    dailyavg = (overalltotal / totaldays)
+    # Chart header
+    drawcenteredtext(draw, "Last 30 days (" + str(int(overalltotal)) + ") Daily Average (" + str(int(dailyavg)) + ")"  , 16, int(width/2), (headerheight + hashheight))
+    if dailyavg < breakevendaily:
+        drawcenteredtext(draw, "Warning: Mining at a Loss.", 16, int(width/2), (headerheight + hashheight - 40), colorFF0000)
+        drawcenteredtext(draw, "Break Even is " + str(breakevendaily) + " sats Daily", 16, int(width/2), (headerheight + hashheight - 20), colorFF0000)
     # Date and Time
     dt = "as of " + getdateandtime()
     drawbottomrighttext(draw, dt, 12, width, height)
