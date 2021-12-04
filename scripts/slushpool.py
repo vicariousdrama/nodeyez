6e5193e838c852381b9d80be2faee4f41165be85
vicariousdrama <88121568+vicariousdrama@users.noreply.github.com>
2021-12-04 23:30:11 +0000

    Calculate earnings today for SlushPool

--- scripts/slushpool.py
+++ scripts/slushpool.py
@@ -8,14 +8,16 @@ import time
 
 outputFile = "/home/bitcoin/images/slushpool.png"
 authtoken = "your-auth-token-here"
-# if the daily average for 30 days drops below this level, then a warning will be displayed
-breakevndaily = 5000
+# experimental. this value here is based on S9 running at 1kWh with electric cost of 12 cents/kWh and a market rate from bisq of $58,000 for 58k gang
+# 100,000,000 sats / $58,000 = 1724 sats/dollar.   1kWh * 24 hours * 12 cents = $2.88 electric cost / day.   1724 * 2.88 is 4965 sats
+# TODO: Fetch the market price and do this calculation dynamically
+breakevendaily = 4965
 colorminingreward=ImageColor.getrgb("#6b50ff")
 colorbosreward=ImageColor.getrgb("#fb82a8")
 colorreferralreward=ImageColor.getrgb("#00bac5")
 colorgraphlinelight=ImageColor.getrgb("#a0a0a0")
 colorgraphlinedark=ImageColor.getrgb("#606060")
-
+colordatavalue=ImageColor.getrgb("#4040ff")
 colorFFFFFF=ImageColor.getrgb("#ffffff")
 color000000=ImageColor.getrgb("#000000")
 colorC0C0C0=ImageColor.getrgb("#c0c0c0")
@@ -32,6 +34,15 @@ def getdateandtime():
     now = datetime.utcnow()
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
+def getaccountprofile():
+    cmd = "curl -H \"SlushPool-Auth-Token: " + authtoken + "\" https://slushpool.com/accounts/profile/json/btc/"
+    try:
+        cmdoutput = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        cmdoutput = "{\"btc\":{\"confirmed_reward\": null, \"unconfirmed_reward\": \"0.00000000\", \"estimated_reward\": \"0.00000000\", \"hash_rate_unit\": \"Gh/s\", \"hash_rate_5m\": 0.0000}}"
+    j = json.loads(cmdoutput)
+    return j
+
 def getaccountrewards():
     time.sleep(6)
     cmd = "curl -H \"SlushPool-Auth-Token: " + authtoken + "\" https://slushpool.com/accounts/rewards/json/btc/"
@@ -42,12 +53,13 @@ def getaccountrewards():
     j = json.loads(cmdoutput)
     return j
 
-def getaccountprofile():
-    cmd = "curl -H \"SlushPool-Auth-Token: " + authtoken + "\" https://slushpool.com/accounts/profile/json/btc/"
+def getpoolstats():
+    time.sleep(6)
+    cmd = "curl -H \"SlushPool-Auth-Token: " + authtoken + "\" https://slushpool.com/stats/json/btc/"
     try:
         cmdoutput = subprocess.check_output(cmd, shell=True).decode("utf-8")
     except subprocess.CalledProcessError as e:
-        cmdoutput = "{\"btc\":{\"confirmed_reward\": null, \"unconfirmed_reward\": \"0.00000000\", \"estimated_reward\": \"0.00000000\", \"hash_rate_unit\": \"Gh/s\", \"hash_rate_5m\": 0.0000}}"
+        cmdoutput = "{\"btc\":{\"blocks\":{\"0\":{\"date_found\":0,\"mining_duration\":0,\"total_shares\":0,\"state\":\"confirmed\",\"confirmations_left\":0,\"value\": \"0.00000000\",\"user_reward\": \"0.00000000\",\"pool_scoring_hash_rate\": 0.000000}}}}"
     j = json.loads(cmdoutput)
     return j
 
@@ -160,30 +172,44 @@ def drawlefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF):
     sh += oy
     draw.text(xy=(x,y-(sh/2)), text=s, font=thefont, fill=textcolor)
 
-def createimage(accountrewards, accountprofile, width=480, height=320):
+def createimage(accountrewards, accountprofile, poolstats, width=480, height=320):
     headerheight = 30
     footerheight = 15
-    hashheight = (height - headerheight - footerheight) * .5
+    hashheight = (height - headerheight - footerheight) * .4
     rewardheight = (height - headerheight - footerheight) * .5
     im = Image.new(mode="RGB", size=(width, height))
     draw = ImageDraw.Draw(im)
     # Header
     drawcenteredtext(draw, "SlushPool Mining Summary", 24, int(width/2), int(headerheight/2))
-    # Hashrate and Unconfirmed rewards
+    # Hashrate
     hashrate = getaccounthashrate(accountprofile)
     drawcenteredtext(draw, "Hashrate", 16, (width/4*1), (headerheight + (hashheight/2) - 24))
-    drawcenteredtext(draw, hashrate, 24, (width/4*1), (headerheight + (hashheight/2)))
-    unconfirmed_reward = float(accountprofile["btc"]["unconfirmed_reward"])
-#    unconfirmed_text = str(format(unconfirmed_reward, ".8f")) + " BTC"
-    unconfirmed_text = str(int(math.floor(unconfirmed_reward * 100000000))) + " sats"
-    drawcenteredtext(draw, "Unconfirmed Rewards", 16, (width/4*3), (headerheight + (hashheight/2) - 24))
-    drawcenteredtext(draw, unconfirmed_text, 24, (width/4*3), (headerheight + (hashheight/2)))
+    drawcenteredtext(draw, hashrate, 24, (width/4*1), (headerheight + (hashheight/2)), colordatavalue)
+    # Yesterday and Today value
+    earningspad = 24
+    value_last_day = "0 sats"
+    value_today = "0 sats"
+    if len(accountrewards["btc"]["daily_rewards"]) > 0:
+        since_date = accountrewards["btc"]["daily_rewards"][0]["date"]
+        value_last_day = str(int(float(accountrewards["btc"]["daily_rewards"][0]["total_reward"]) * 100000000)) + " sats"
+        todaytally = 0
+        for key in poolstats["btc"]["blocks"]:
+            block = poolstats["btc"]["blocks"][key]
+            if block["date_found"] > since_date:
+                todaytally = todaytally + int(float(block["user_reward"]) * 100000000)
+            else:
+                break
+        value_today = str(todaytally) + " sats"
+    drawcenteredtext(draw, "Earnings Yesterday", 16, (width/4*3), (headerheight + (hashheight/2) - 24 - earningspad))
+    drawcenteredtext(draw, value_last_day, 24, (width/4*3), (headerheight + (hashheight/2) - earningspad), colordatavalue)
+    drawcenteredtext(draw, "Earnings Today", 16, (width/4*3), (headerheight + (hashheight/2) - 24 + earningspad))
+    drawcenteredtext(draw, value_today, 24, (width/4*3), (headerheight + (hashheight/2) + earningspad), colordatavalue)
+
     # 30 Days Rewards
     highestreward = gethighestreward(accountrewards)
     lowestreward = getlowestreward(accountrewards)
     labelwidth = math.floor(width / 5)
     graphedge = 3
-#    drawtoplefttext(draw, "Rewards in last 30 days", 16, 0, (headerheight + hashheight))
     charttop = headerheight + hashheight + 24
     chartleft = labelwidth + graphedge
     chartright = width - graphedge
@@ -202,12 +228,6 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
         draw.line(xy=[i,chart50,i+1,chart50],fill=colorgraphlinedark,width=1)
         draw.line(xy=[i,chart75,i+1,chart75],fill=colorgraphlinedark,width=1)
     # - left labels
-#    reward25 = lowestreward + ((highestreward - lowestreward)/4*3)
-#    reward50 = lowestreward + ((highestreward - lowestreward)/4*2)
-#    reward75 = lowestreward + ((highestreward - lowestreward)/4*1)
-#    drawrighttext(draw, str(format(reward25, ".5f")) + " BTC", 12, labelwidth, chart25)
-#    drawrighttext(draw, str(format(reward50, ".5f")) + " BTC", 12, labelwidth, chart50)
-#    drawrighttext(draw, str(format(reward75, ".5f")) + " BTC", 12, labelwidth, chart75)
     reward25 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*3)) * 100000000))
     reward50 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*2)) * 100000000))
     reward75 = int(math.floor((lowestreward + ((highestreward - lowestreward)/4*1)) * 100000000))
@@ -231,14 +251,19 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
         if highestreward > lowestreward:
             barpct = (currenttotal-lowestreward)/(highestreward-lowestreward)
         bartop = chartbottom - int(math.floor((chartbottom-charttop)*barpct))
-        draw.rectangle(xy=[dayx,bartop,dayx+barwidth,chartbottom-1],fill=colorminingreward)       
+        draw.rectangle(xy=[dayx,bartop,dayx+barwidth,chartbottom-1],fill=colorminingreward)
     overalltotal = overalltotal * 100000000
-    dailyavg = (overalltotal / totaldays)
     # Chart header
-    drawcenteredtext(draw, "Last 30 days (" + str(int(overalltotal)) + ") Daily Average (" + str(int(dailyavg)) + ")"  , 16, int(width/2), (headerheight + hashheight))
-    if dailyavg < breakevendaily:
-        drawcenteredtext(draw, "Warning: Mining at a Loss.", 16, int(width/2), (headerheight + hashheight - 40), colorFF0000)
-        drawcenteredtext(draw, "Break Even is " + str(breakevendaily) + " sats Daily", 16, int(width/2), (headerheight + hashheight - 20), colorFF0000)
+    if days > 0:
+        dailyavg = (overalltotal / days)
+        # Warn if missing breakeven. TODO: Calculate dynamically based on electric cost vs bisq market rates
+        if dailyavg < breakevendaily:
+            drawcenteredtext(draw, "Warning: Mining at a Loss.", 16, int(width/2), (headerheight + hashheight - 40), colorFF0000)
+            drawcenteredtext(draw, "Break Even is " + str(breakevendaily) + " sats Daily", 16, int(width/2), (headerheight + hashheight - 20), colorFF0000)
+        drawcenteredtext(draw, "Last 30 days (" + str(int(overalltotal)) + ") Daily Average (" + str(int(dailyavg)) + ")"  , 16, int(width/2), (headerheight + hashheight))
+    else:
+        drawcenteredtext(draw, "Rewards will be graphed below once earnings are recorded"  , 16, int(width/2), (headerheight + hashheight))
+
     # Date and Time
     dt = "as of " + getdateandtime()
     drawbottomrighttext(draw, dt, 12, width, height)
@@ -249,5 +274,6 @@ def createimage(accountrewards, accountprofile, width=480, height=320):
 while True:
     accountprofile = getaccountprofile()
     accountrewards = getaccountrewards()
-    createimage(accountrewards,accountprofile)
-    time.sleep(300)
+    poolstats = getpoolstats()
+    createimage(accountrewards,accountprofile,poolstats)
+    time.sleep(600)
