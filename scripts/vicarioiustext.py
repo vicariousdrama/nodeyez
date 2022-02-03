09eccfb0e1f3aa851d01dd1c8ba1ea9f21a22dbb
Vic <88121568+vicariousdrama@users.noreply.github.com>
2022-02-03 16:05:17 +0000

    module for text operations

---
+++ scripts/vicarioiustext.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python3
+
+from datetime import datetime
+from PIL import ImageColor, ImageFont
+import time
+
+colorFFFFFF=ImageColor.getrgb("#ffffff")
+
+def getdateandtime():
+    now = datetime.utcnow()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+def getfont(size, isbold=False):
+    if isbold:
+        return ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",size)
+    else:
+        return ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",size)
+
+def drawcenteredtext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x-(sw/2),y-(sh/2)), text=s, font=thefont, fill=textcolor)
+
+def drawbottomlefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x,y-sh), text=s, font=thefont, fill=textcolor)
+
+def drawbottomrighttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x-sw,y-sh), text=s, font=thefont, fill=colorFFFFFF)
+
+def drawtoplefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x,y), text=s, font=thefont, fill=textcolor)
+
+def drawtoprighttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x-sw,y), text=s, font=thefont, fill=textcolor)
+
+def drawrighttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x-sw,y-(sh/2)), text=s, font=thefont, fill=textcolor)
+
+def drawlefttext(draw, s, fontsize, x, y, textcolor=colorFFFFFF, isbold=False):
+    thefont = getfont(fontsize, isbold)
+    sw,sh = draw.textsize(s, thefont)
+    ox,oy = thefont.getoffset(s)
+    sw += ox
+    sh += oy
+    draw.text(xy=(x,y-(sh/2)), text=s, font=thefont, fill=textcolor)
