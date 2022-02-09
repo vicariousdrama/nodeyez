a63a6e966ec4d58d22a85afebafbc6900e895eb8
Vicarious Drama <vdgit@lucasmoten.com>
2022-02-08 20:19:05 -0600

    Support for Compass Mining Reseller Marketplace

--- scripts/compassmininghardware.py
+++ scripts/compassmininghardware.py
@@ -1,56 +1,175 @@
 #! /usr/bin/python3
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageColor
-from bs4 import BeautifulSoup
+from os.path import exists
+import json
 import math
 import subprocess
+import sys
 import time
 import requests
 import vicarioustext
 
-# Depends on 
-#    pip install beautifulsoup4
 
-# https://us-central1-hashr8-compass.cloudfunctions.net/app/hardware/group?isWeb=true&sortByCost=asc
+def getHardwareInfo():
+    cmd = "curl -s " + hardwareurl
+    try:
+        cmdoutput = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        print(f"Error retrieving compass mining hardware data from {hardwareurl}")
+        print(f"{e}")
+        cmdoutput = '{"ok":true,"payload":{"hardwareGrouped":[]}}'
+    j = json.loads(cmdoutput)
+    return j
 
-outputFile="/home/bitcoin/images/compassmininghardware.png"
-infourl="https://compassmining.io/hardware"
-colorFFFFFF=ImageColor.getrgb("#ffffff")
+def getHardwareGrouped(hwinfo):
+    if "payload" in hwinfo:
+        if "hardwareGrouped" in hwinfo["payload"]:
+            hardwareGrouped = hwinfo["payload"]["hardwareGrouped"]
+            if len(hardwareGrouped) > 0:
+                return hardwareGrouped
+    return None
 
-def getinfopage():
-    page = requests.get(infourl)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return soup
+def findLowestPrice(hwinfo):
+    matchentry = hwinfo[0]
+    for hwentry in hwinfo:
+        if hwentry["algorithm"] == "SHA256":
+            if int(hwentry["minCost"]) < int(matchentry["minCost"]):
+                matchentry = hwentry
+    return matchentry
 
+def findHighestHashrate(hwinfo):
+    matchentry = hwinfo[0]
+    for hwentry in hwinfo:
+        if hwentry["algorithm"] == "SHA256":
+            if int(hwentry["hashrate"]) > int(matchentry["hashrate"]):
+                matchentry = hwentry
+    return matchentry
+
+def findCheapestHashrate(hwinfo):
+    matchentry = hwinfo[0]
+    for hwentry in hwinfo:
+        if hwentry["algorithm"] == "SHA256":
+            if float(int(hwentry["minCost"])) / float(int(hwentry["hashrate"])) < float(int(matchentry["minCost"])) / float(int(matchentry["hashrate"])):
+                matchentry = hwentry
+    return matchentry
+
+def findEfficientHashrate(hwinfo):
+    matchentry = hwinfo[0]
+    for hwentry in hwinfo:
+        if hwentry["algorithm"] == "SHA256":
+            if float(int(hwentry["power"])) / float(int(hwentry["hashrate"])) < float(int(matchentry["power"])) / float(int(matchentry["hashrate"])):
+                matchentry = hwentry
+    return matchentry
+
+def renderEntry(draw, x, y, w, h, label, entry):
+    name = entry["name"]
+    hashrate = entry["hashrate"]
+    power = entry["power"]
+    minCost = entry["minCost"]
+    eff = round((float(power) / float(hashrate)),1)
+    costPerTH = round((float(minCost) / float(hashrate)),2)
+    # e.g.   Lowest Price
+    vicarioustext.drawcenteredtext(draw, label, sizeEntryLabel, x + (w/2), y+20, colorEntryLabel)
+    # e.g.   Whatsminer M32 66TH/s
+    vicarioustext.drawcenteredtext(draw, name + " " + str(hashrate) + "TH", sizeEntryName, x + (w/2), y+40, colorEntryName)
+    # e.g.   from $6999
+    vicarioustext.drawcenteredtext(draw, "from $" + str(minCost), sizeEntryInfo, x + (w/2), y+60, colorEntryPrice, True)
+    # e.g.   3300 watt (50.0J/TH)
+    vicarioustext.drawcenteredtext(draw, str(power) + " watt (" + str(eff) + "J/TH)", sizeEntryInfo, x + (w/2), y+80, colorEntryPower)
+    # e.g.   $106.05/TH
+    vicarioustext.drawcenteredtext(draw, "$" + str(costPerTH) + "/TH", sizeEntryInfo, x + (w/2), y+100, colorEntryPower)
 
 def createimage(width=480, height=320):
-    soup = getinfopage()
-    headerheight = 30
+    hardwareinfo = getHardwareInfo()
+    hwGrouped = getHardwareGrouped(hardwareinfo)
+    headerheight = 50
+    footerheight = 18
     im = Image.new(mode="RGB", size=(width, height))
     draw = ImageDraw.Draw(im)
     # header
-    vicarioustext.drawcenteredtext(draw, "Compass Mining Hardware Prices", 24, int(width/2), int(headerheight/2), colorFFFFFF, True)
-    # incidents
-    incidentcount = 0
-    incidentrowheight = 40
-    # isolate entries
-    entries = soup.find_all(class_="bg-white")
-    for entry in entries:
-        print(entry.name)
-        if entry.name.lower() == "section":
-            sibling = entry.find_next_sibling()
-            if sibling.name.lower() == "div":
-                miner_name = entry.get_text()
-                miner_price = sibling.find(class_="text-base").get_text()
-                print(miner_name + " " + miner_price + "\n")
+    vicarioustext.drawcenteredtext(draw, "Compass Mining Hardware Prices", 24, int(width/2), 15, colorTextFG, True)
+    vicarioustext.drawcenteredtext(draw, "Certified Reseller ASICs", 16, int(width/2), 40, colorTextFG)
 
-
+    if hwGrouped is not None:
+        # find the machines we want
+        entryLowestPrice = findLowestPrice(hwGrouped)
+        entryHighestHashrate = findHighestHashrate(hwGrouped)
+        entryCheapestHashrate = findCheapestHashrate(hwGrouped)
+        entryEfficientHashrate = findEfficientHashrate(hwGrouped)
+        # render
+        internalHeight = height-(headerheight+footerheight)
+        halfInternalHeight = int(internalHeight/2)
+        renderEntry(draw, 0, headerheight, int(width/2), halfInternalHeight, "Lowest Price", entryLowestPrice)
+        renderEntry(draw, int(width/2), headerheight, int(width/2), halfInternalHeight, "Highest Hashrate", entryHighestHashrate)
+        renderEntry(draw, 0, headerheight + halfInternalHeight, int(width/2), halfInternalHeight, "Cheapest Hashrate", entryCheapestHashrate)
+        renderEntry(draw, int(width/2), headerheight + halfInternalHeight, int(width/2), halfInternalHeight, "Energy Efficient", entryEfficientHashrate)
+    else:
+        vicarioustext.drawcenteredtext(draw, "Unable to get hardware information", 20, width, height, colorTextWarn)
 
     # timestamp
-    vicarioustext.drawbottomrighttext(draw, "as of " + vicarioustext.getdateandtime(), 12, width, height)
+    vicarioustext.drawbottomrighttext(draw, "as of " + vicarioustext.getdateandtime(), 12, width, height, colorTextFG)
     im.save(outputFile)
 
-#while True:
-#    createimage()
-#    time.sleep(86400)
-createimage()
+
+
+if __name__ == '__main__':
+    # Defaults
+    configFile="/home/bitcoin/nodeyez/config/compassmininghardware.json"
+    outputFile="/home/bitcoin/images/compassmininghardware.png"
+    hardwareurl="https://us-central1-hashr8-compass.cloudfunctions.net/app/hardware/group?isWeb=true&sortByCost=asc"
+    colorTextFG=ImageColor.getrgb("#ffffff")
+    colorTextWarn=ImageColor.getrgb("#ff0000")
+    colorEntryLabel=ImageColor.getrgb("#8080ff")
+    colorEntryName=ImageColor.getrgb("#ff8000")
+    colorEntryPrice=ImageColor.getrgb("#ffffff")
+    colorEntryPower=ImageColor.getrgb("#808080")
+    sizeEntryLabel=20
+    sizeEntryName=18
+    sizeEntryInfo=14
+    sleepInterval=3600
+    # Override defaults
+    if exists(configFile):
+        with open(configFile) as f:
+            config = json.load(f)
+        if "compassmininghardware" in config:
+            config = config["compassmininghardware"]
+        if "outputFile" in config:
+            outputFile = config["outputFile"]
+        if "hardwareurl" in config:
+            hardwareurl = config["hardwareurl"]
+        if "colorTextFG" in config:
+            colorTextFG = ImageColor.getrgb(config["colorTextFG"])
+        if "colorTextWarn" in config:
+            colorTextWarn = ImageColor.getrgb(config["colorTextWarn"])
+        if "colorEntryLabel" in config:
+            colorEntryLabel = ImageColor.getrgb(config["colorEntryLabel"])
+        if "colorEntryName" in config:
+            colorEntryName = ImageColor.getrgb(config["colorEntryName"])
+        if "colorEntryPrice" in config:
+            colorEntryPrice = ImageColor.getrgb(config["colorEntryPrice"])
+        if "colorEntryPower" in config:
+            colorEntryPower = ImageColor.getrgb(config["colorEntryPower"])
+        if "sizeEntryLabel" in config:
+            sizeEntryLabel = int(config["sizeEntryLabel"])
+        if "sizeEntryName" in config:
+            sizeEntryName = int(config["sizeEntryName"])
+        if "sizeEntryInfo" in config:
+            sizeEntryInfo = int(config["sizeEntryInfo"])
+        if "sleepInterval" in config:
+            sleepInterval = int(config["sleepInterval"])
+            sleepInterval = 300 if sleepInterval < 300 else sleepInterval # minimum 5 minutes, access other
+    # Check for single run
+    if len(sys.argv) > 1:
+        if sys.argv[1] in ['-h','--help']:
+            print(f"Retrieves the Compass Mining Hardware from reseller market and summarizes")
+            print(f"Usage:")
+            print(f"1) Call without arguments to run continuously using the configuration or defaults")
+            print(f"You may specify a custom configuration file at {configFile}")
+        else:
+            createimage()
+        exit(0)
+    # Loop
+    while True:
+        createimage()
+        time.sleep(sleepInterval)
