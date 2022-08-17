e210f49fa2ef4450bbb5e9391b692c3285b56f17
Vicarious Drama <vdgit@lucasmoten.com>
2022-08-17 09:17:13 -0500

    Support image in Halving panel

--- scripts/halving.py
+++ scripts/halving.py
@@ -1,22 +1,49 @@
 #! /usr/bin/env python3
-from PIL import Image, ImageDraw, ImageColor
+from PIL import Image, ImageDraw, ImageColor, ImageOps
 from os.path import exists
 import json
 import math
+import os
+import random
 import subprocess
 import sys
 import time
 import vicariousbitcoin
 import vicarioustext
 
+def getGridImage(p):
+    if os.path.exists(ipfsDirectory):
+        random.seed(p * 11235813)
+        filegood = False
+        fileattempts = 5
+        while ((not filegood) and (fileattempts > 0)):
+            filename = random.choice(os.listdir(ipfsDirectory))
+            try:
+                filepath = ipfsDirectory + "/" + filename
+                filesize = os.path.getsize(filepath)
+                if filesize > 50000:
+                    rImage = Image.open(filepath).convert("RGBA")
+                    filegood = True
+            except BaseException as err:
+                # not sure what
+                print(f"meh error: {err}")
+                fileattempts = fileattempts -1
+        if filegood:
+            return rImage
+    # default
+    return Image.open("/home/nodeyez/nodeyez/images/logo.png").convert("RGBA")
+
+
 def createimage(width=480, height=320):
+    rows = 30
+    cols = 70
     nHeight = vicariousbitcoin.getcurrentblock()
-    nSubsidyHalvingInterval = 210000
+    nSubsidyHalvingInterval = rows * cols * 100 # 210000
     halvings = nHeight / nSubsidyHalvingInterval
     halvingbegin = math.floor(halvings) * nSubsidyHalvingInterval
     halvingend = halvingbegin + nSubsidyHalvingInterval - 1
     halvingpct = float(nHeight - halvingbegin) / float(2100.00)
-    gridblocks = nHeight % 2100
+    gridblocks = nHeight % (rows * cols) # 2100
     # start the image with header and footer
     padtop=36
     im = Image.new(mode="RGB", size=(width, height), color=colorBackground)
@@ -29,7 +56,7 @@ def createimage(width=480, height=320):
     draw.rounded_rectangle(xy=(padleft,padtop,width-padleft,padtop+barheight),radius=3,fill=None,outline=colorGrid,width=1)
     barwidth=int(math.floor(float(width-(padleft+padleft+4))*halvingpct/100.00))
     draw.rounded_rectangle(xy=(padleft+2,padtop+2,padleft+barwidth,padtop+barheight-2),radius=3,fill=colorProgress)
-    pcttxt = str(format(halvingpct, ".2f")) + "%"
+    pcttxt = str(format(halvingpct, ".3f")) + "%"
     if halvingpct < 25:
         vicarioustext.drawcenteredtext(draw, str(nHeight) + " is " + pcttxt, 18, (width/2), padtop+(barheight/2), colorProgress, True)
     elif halvingpct < 75:
@@ -38,11 +65,29 @@ def createimage(width=480, height=320):
     else:
         vicarioustext.drawcenteredtext(draw, str(nHeight) + " is " + pcttxt, 18, (width/2), padtop+(barheight/2), colorBackground, True)
     # grid of 70x30 (2100) for the current percent
-    rows = 30
-    cols = 70
     blockw=int(math.floor(width/cols))
     padleft=int(math.floor((width-(cols*blockw))/2))
     padtop=100
+    if gridImageEnabled:
+        gridw = blockw * cols
+        gridh = blockw * rows
+        gridratio=float(gridw)/float(gridh)
+        gridsource = getGridImage(int(halvingpct))
+        gridsourcew = int(gridsource.getbbox()[2])
+        gridsourceh = int(gridsource.getbbox()[3])
+        gridsourceratio = float(gridsourcew)/float(gridsourceh)
+        if gridsourceratio > gridratio:
+            newgridsourceheight=int(gridsourcew/gridratio)
+            gridsourceoffset = int((newgridsourceheight-gridsourceh)/2)
+            gridsourcetaller = Image.new(mode="RGBA", size=(gridsourcew, newgridsourceheight), color=colorProgress)
+            gridsourcetaller.paste(gridsource, (0,gridsourceoffset))
+            gridimage = gridsourcetaller.resize(size=(gridw,gridh))
+        else:
+            newgridsourcewidth=int(gridsourceh*gridratio)
+            gridsourceoffset = int((newgridsourcewidth-gridsourcew)/2)
+            gridsourcewider = Image.new(mode="RGBA", size=(newgridsourcewidth, gridsourceh), color=colorProgress)
+            gridsourcewider.paste(gridsource, (gridsourceoffset,0))
+            gridimage = gridsourcewider.resize(size=(gridw,gridh))
     for dc in range(cols):
         for dr in range(rows):
             gridblocknum = ((dr*cols)+dc)+1
@@ -55,18 +100,43 @@ def createimage(width=480, height=320):
             if gridblocknum <= gridblocks:
                 fillcolor = colorProgress
                 outlinecolor = None
-            draw.rectangle(xy=((tlx,tly),(brx,bry)),fill=fillcolor,outline=outlinecolor)
+            if gridImageEnabled:
+                # crop from gridimage
+                gtlx = (dc*blockw)
+                gtly = (dr*blockw)
+                gbrx = gtlx+blockw-1
+                gbry = gtly+blockw-1
+                gblockimg = gridimage.crop((gtlx,gtly,gbrx,gbry))
+                if fillcolor != colorProgress:
+                    # convert to black and white
+                    if gridImageUnminedMode == 'grayscale':
+                        gblockimg = ImageOps.grayscale(gblockimg)
+                    if gridImageUnminedMode == 'dither':
+                        gblockimg = gblockimg.convert('1')
+                    if gridImageUnminedMode == 'dither2':
+                        gblockimg = gblockimg.convert('L')
+                # paste the part into the right spot
+                im.paste(gblockimg, (tlx, tly))
+                if gridblocknum == gridblocks:
+                    fillcolor = None
+                    outlinecolor = colorProgress
+                    draw.rectangle(xy=((tlx-1,tly-1),(brx+1,bry+1)),fill=fillcolor,outline=outlinecolor,width=2)
+            else:
+                draw.rectangle(xy=((tlx,tly),(brx,bry)),fill=fillcolor,outline=outlinecolor)
     im.save(outputFile)
 
 
 if __name__ == '__main__':
     # Defaults
     configFile="/home/nodeyez/nodeyez/config/halving.json"
-    outputFile="/home/nodeyez/nodeyez/imageoutput/halving.png"
+    outputFile="/home/nodeyez/nodeyez/imageoutput/zhalving.png"
+    ipfsDirectory="/home/nodeyez/nodeyez/data/ipfs"
     colorGrid=ImageColor.getrgb("#404040")
     colorProgress=ImageColor.getrgb("#40FF40")
     colorTextFG=ImageColor.getrgb("#ffffff")
     colorBackground=ImageColor.getrgb("#000000")
+    gridImageEnabled=True
+    gridImageUnminedMode="grayscale"
     width=480
     height=320
     sleepInterval=540
@@ -86,6 +156,12 @@ if __name__ == '__main__':
             colorTextFG = ImageColor.getrgb(config["colorTextFG"])
         if "colorBackground" in config:
             colorBackground = ImageColor.getrgb(config["colorBackground"])
+        if "gridImageEnabled" in config:
+            gridImageEnabled = config["gridImageEnabled"]
+        if "gridImageUnminedMode" in config:
+            gridImageUnminedMode = config["gridImageUnminedMode"]
+        if "ipfsDirectory" in config:
+            ipfsDirectory = config["ipfsDirectory"]
         if "width" in config:
             width = int(config["width"])
         if "height" in config:
